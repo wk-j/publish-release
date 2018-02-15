409b8672151ea630630746a6760ba535c5eb6641
wk-j <somnuk.wk@gmail.com>
2018-02-16 02:11:07 +0700

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Publish Release  [![Build Status](https://travis-ci.org/wk-j/publish-release.svg?branch=master)](https://travis-ci.org/wk-j/publish-release)
---
+++ src/PublishRelease/Release.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace PublishRelease {
+
+    public class CommandLineOptions {
+        public string Name { set; get; } = "";
+
+        public string Owner { set; get; } = "";
+
+        public string Repo { set; get; } = "";
+
+        public string Tag { set; get; } = "";
+
+        public bool Draft { set; get; }
+
+        public bool Prerelease { set; get; }
+
+        public string[] Assets { set; get; } = { };
+    }
+
+    public class CreateReleaseResponse {
+        [JsonProperty("url")]
+        public string Url { set; get; }
+
+        [JsonProperty("upload_url")]
+        public string UploadUrl { set; get; }
+    }
+
+    public class UploadAssetReponse {
+        [JsonProperty("url")]
+        public string Url { set; get; }
+    }
+
+    public static class Release {
+        public static void CreateRelease(CommandLineOptions options) {
+            using (var client = new HttpClient()) {
+
+            }
+        }
+    }
+}
